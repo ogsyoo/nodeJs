492c77d5ee57811617ca6d6c2a1f354be0b8294a
shiyao <shiyao@spacesystech.com>
2017-12-09 12:36:53 +0800

    前端发布测试

--- app.js
+++ app.js
@@ -11,21 +11,22 @@ var app = express();
 const fs = require('fs');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', function(req, res) {
-  const html = fs.readFileSync(path.resolve(__dirname, 'public/dist/index.html'), 'utf-8')
+app.use(express.static(path.join(__dirname, './public/dist')));
+app.get('/', function(req, res) {
+  const html = fs.readFileSync(path.resolve(__dirname, './public/dist/index.html'), 'utf-8')
+  console.log(html);
   res.send(html)
 })
-// app.use('/', index);
+app.use("/api",index);
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,8 @@ router.post('/login', function (req, res) {
     var data = {name:name,password:password};
     weRes.exportJson(res,null,data);
 });
+router.get('/gsy',(req,res) => {
+    // 通过模型去查找数据库
+   res.send("gsy123124214");
+});
 module.exports = router;
