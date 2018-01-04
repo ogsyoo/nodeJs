63836029f7a58a2607ca4d12b527de7e8e9489be
gaoshiyao <shiyao@spacesystech.com>
2018-01-04 17:49:54 +0800

    redis String 操作

--- dao/redis_dao.js
+++ dao/redis_dao.js
@@ -1,12 +1,38 @@
 var redis = require('redis');
 var db = require('../common/db/redis_db');
 var async = require('async');
-client = redis.createClient(db.RDS_PORT, db.RDS_HORT, db.RDS_OPTS);
+var client;
 var that = {
+    /**
+     * 连接redis数据库
+     * @param cb
+     */
+    connect: function (cb) {
+        if (!client) {
+            client = redis.createClient(db.RDS_PORT, db.RDS_HORT, db.RDS_OPTS);
+            client.on('error', function (error) {
+                console.log("连接错误");
+                cb(error);
+            });
+            client.on('ready', function (err) {
+                console.log("连接成功");
+                if (err) cb(err);
+                cb(null, "连接成功");
+            });
+        } else {
+            console.log("已连接");
+            cb(null, "已连接");
+        }
+    },
+    /**
+     * 选择数据库
+     * @param dbNo
+     * @param cb
+     */
     slectDb: function (dbNo, cb) {
-        client.select(slectDb, function (err) {
+        client.select(dbNo, function (err, response) {
             if (err) cb(err);
-            cb(null);
+            cb(null, response);
         })
     },
     /**
@@ -15,14 +41,39 @@ var that = {
      * @param params
      * @param cb
      */
-    set: function (slectDb, params, cb) {
-        for (var key in params) {
-            client.set(key, params[key], function (err) {
-                if(err)
-                    cb(err);
-            });
+    setString: function (key, value, cb) {
+        client.set(key, value, function (err, response) {
+            if (err) cb(err);
+            cb(null, "插入成功");
+        });
+    },
+    /**
+     * string 设置多个数据
+     * @param params
+     * @param cb
+     */
+    setAll: function (params, cb) {
+        if (params) {
+            for (let key in params) {
+                client.set(key, params[key], function (err) {
+                    if (err)
+                        cb(err);
+                });
+            }
+            cb(null,"插入成功");
         }
-        cb(null,"ok");
+    },
+    /**
+     * string 获取数据
+     * @param key
+     * @param cb
+     */
+    getString:function (key,cb) {
+        client.get(key,function (err,response) {
+            if (err)
+                cb(err);
+            cb(null,response);
+        });
     }
 }
 module.exports = that;
--- routes/redis.js
+++ routes/redis.js
@@ -3,7 +3,20 @@ var router = express.Router();
 var weRes = require('../common/util/weRes');
 var app = express();
 var redisService = require('../service/redisService');
-router.get("/test",function(req,res){
-    redisService.setAuthor(weRes.exportJson.bind(null,res));
+router.get("/setAuthor", function (req, res) {
+    var setParams = {
+        dbNo: 1,
+        userName: "gsy",
+        passWord: "5buhui3xin"
+    };
+    redisService.setAuthor(setParams,weRes.exportJson.bind(null, res));
 });
-module.exports=router;
+router.get("/getAuthor", function (req, res) {
+    var getParams = {
+        dbNo: 1,
+        userName: "gsy",
+    }
+    redisService.getAuthor(getParams,weRes.exportJson.bind(null, res));
+});
+
+module.exports = router;
